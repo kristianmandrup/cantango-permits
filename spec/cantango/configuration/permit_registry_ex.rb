57174716bcb552f80a8774bb0bac167aa7e1ca8f
Kristian Mandrup <kmandrup@gmail.com>
2011-11-21 21:29:22 +0100

    improved config api

--- spec/cantango/configuration/permit_registry_ex.rb
+++ spec/cantango/configuration/permit_registry_ex.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 shared_examples_for CanTango::Configuration::PermitRegistry do
   it "should respond to permit type methods" do
-    [:user_type, :account_type, :role, :role_group].each do |permit_type|
-      subject.get(permit_type).should_not be_nil 
+    [:user_type, :account_type, :role, :role_group].each do |type|
+      subject.get_permit(type).should_not be_nil 
     end
   end
 end
