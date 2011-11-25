3d1e910c48b362d293ad4a04c018fb8b61c6a8bc
Kristian Mandrup <kmandrup@gmail.com>
2011-11-25 16:59:24 +0100

    added cantango classlv permits debug methods

---
+++ lib/cantango/class_methods.rb
@@ -0,0 +1,27 @@
+module CanTango
+  module ClassMethods
+    def permits_allowed candidate, actions, subjects, *extra_args
+      raise "Debugging has not been turned on. Turn it on using: CanTango.debug!" if CanTango.config.debug.off?
+      config.permits.allowed candidate, actions, subjects, *extra_args
+    end
+
+    def permits_denied candidate, actions, subjects, *extra_args
+      raise "Debugging has not been turned on. Turn it on using: CanTango.debug!" if CanTango.config.debug.off?
+      config.permits.denied candidate, actions, subjects, *extra_args
+    end
+
+    def debug_permits_registry
+      puts "permits registry:" << CanTango.config.permits.show_all.inspect
+    end
+
+    def debug_ability candidate, actions, subjects, *extra_args
+      puts "Ability: #{actions} on #{subjects}"
+      puts "permits allowed:" << permits_allowed(candidate, actions, subjects, *extra_args).inspect
+      puts "permits denied:"  << permits_denied(candidate, actions, subjects, *extra_args).inspect
+    end
+
+    def clear_permits_executed!
+      config.permits.clear_executed!
+    end
+  end
+end
--- lib/cantango/permits.rb
+++ lib/cantango/permits.rb
@@ -1,6 +1,9 @@
+require 'cantango/class_methods'
+
 module CanTango
   autoload_modules :Builder, :Factory, :Finder, :License, :Permit
   autoload_modules :Engine, :Executor, :Macros
+  autoload_modules :ClassMethods
 end
 
 [:user_type, :account_type, :role, :role_group, :special].each do |permit|
